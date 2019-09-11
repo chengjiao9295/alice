734c229f2a3eecb7731013c980f3bf1f5cf895b3
Alice <2664436347@qq.com>
2019-09-11 17:21:21 +0800

    update

--- web/src/main/webapp/front/js/login.js
+++ web/src/main/webapp/front/js/login.js
@@ -1,38 +1,44 @@
   (function($){
-     $('button.submit').click(function (ev) {
-           $.ajax({
-                url:'members/select/testPathVariable',
-                type:'GET',
-                dataType:"JSON",
-                data:{'name':'da'},
-                success:function(data) {
+//     $('button.submit').click(function (ev) {
+//           $.ajax({
+//                url:'members/select/testPathVariable',
+//                type:'GET',
+//                dataType:"JSON",
+//                data:{'name':'da'},
+//                success:function(data) {
+//
+//                   console.log(data);
+//                   alert(data);
+//
+//                }
+//            });
+//
+//        });
 
-                   console.log(data);
-                   alert(data);
+      var user = function(){
+             $.ajax({
+                 url:'userInfo/select',
+                 type:'GET',
+                 success:function(resp) {
+                     console.log(resp);
+                     render(resp);
+                     },
+                 error:function(){
+                    console.log("fail");
+                    }
+             });
+      };
 
-                }
-            });
+      $(document.getElementsByTagName("button")).click(function (ev){
+           user();
+      });
 
-        });
-
-          var user = function(){
-                 $.ajax({
-                     url:'userInfo/select',
-                     type:'GET',
-                     success:function(resp) {
-                         console.log(resp);
-                         alert(resp);
-                             if(resp){
-                                 console.log(resp);
-                             }
-                         },
-                     error:function(){
-                        console.log("fail");
-                     }
-                 });
-            }
-
-            $(document.getElementsByTagName("button")).click(function (ev){
-               user();
-            });
+      var render = function(resp){
+        var name = document.getElementsByClassName("name");
+        var email = document.getElementsByClassName("email");
+        var pwd = document.getElementsByClassName("pwd");
+        $(name).val(resp.name);
+        $(email).val(resp.email);
+        $(pwd).val(resp.pwd);
+      };
   }($));
